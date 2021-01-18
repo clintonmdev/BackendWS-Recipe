8bbeabc600223507a72ecb95a57414a7f1680314
Clinton <Clintonm.pro@gmail.com>
2021-01-18 22:04:16 +0100

    added create ingredient feater

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,7 +25,10 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewset(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewset(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+
     """ Manage ingredients in the database """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -36,3 +39,7 @@ class IngredientViewset(viewsets.GenericViewSet, mixins.ListModelMixin):
         """Return queryset for the authenticated user"""
         return Ingredient.objects.filter(
             user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
