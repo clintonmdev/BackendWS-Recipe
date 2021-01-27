1bbed7fddc7396359f8fab3862dcf7cffe981867
Clinton <Clintonm.pro@gmail.com>
2021-01-27 16:41:53 +0100

    added filtering features for tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,14 @@ class BaseRecipeAttrViewset(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return queryset for authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create new object """
